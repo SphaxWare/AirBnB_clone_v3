e4b18d769694299f32708bee2202665bf9ff69ab
Sphax <itssphax123@gmail.com>
2024-03-30 08:54:11 -0700

    3. Status of your API

---
+++ api/v1/app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""App.py for api v1"""
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+import os
+
+
+app = Flask(__name__)
+
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """
+    Teardown application context
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    app.run(host=host, port=port, threaded=True)
