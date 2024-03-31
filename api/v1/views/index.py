07ac279b24b992f83c5d2cda885c2070c9f654f1
SphaxWare <134861101+SphaxWare@users.noreply.github.com>
2024-03-31 05:25:54 +0000

    Update index.py method get

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,7 @@ from flask import jsonify
 from . import app_views
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status', methods=['GET'])
 def get_status():
     """Return API Status"""
     return jsonify({"status": "OK"})
